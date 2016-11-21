13f7ee2b05a05ab2344ff18e4cccb09d6777a130
Eduardo <eduardo_demoura@yahoo.com.br>
2016-11-21 09:22:43 -0200

    Update Consulado.py

--- VBScript/ConsuladoPortugal/Consulado.py
+++ VBScript/ConsuladoPortugal/Consulado.py
@@ -8,17 +8,36 @@
 # Copyright (C) 2016
 # ---------------------------------------------------------------------------------------
 
+
 from urllib import urlencode
 from urllib2 import Request, urlopen
 import lxml.html
 
-url = 'http://www.consuladoportugalrjservicos.com/public_html/exec' # Set destination URL here
-values = { 'modulo': 'modulo.servicos', 'acao': 'servico902', 'opcao': '', 'idpedido': '376520', 'indexboleto': '0' }     # Set POST fields here
+# Set destination URL here
+url = 'http://www.consuladoportugalrjservicos.com/public_html/exec'
+
+# Set POST fields here
+values = {
+    'modulo': 'modulo.servicos',
+    'acao': 'servico902',
+    'opcao': '',
+    'idpedido': '376520',
+    'indexboleto': '0'
+}
+
 data = urlencode(values)
-headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': str(len(data))}
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': str(len(data)),
+    'Cookie': 'JSESSIONID=122FD45D1B35ABD18992B24680C19B37'
+}
+
 request = Request(url, data, headers)
+
 response = urlopen(request).read()
+
 root = lxml.html.fromstring(response)
+print(response)
 for elem in root.cssselect('elem.class'):
     print(elem.tag)
     print(elem.get('dtycampo'))
