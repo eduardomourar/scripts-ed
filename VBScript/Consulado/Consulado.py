1b459a99cc976d4526b0135a4302bbe32f450d5e
Eduardo de Moura Rodrigues <eduardomourar@users.noreply.github.com>
2016-12-31 22:11:28 -0200

    Update Consulado.py

--- VBScript/Consulado/Consulado.py
+++ VBScript/Consulado/Consulado.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 # ---------------------------------------------------------------------------------------
 # Name      : Acessar Consulado
-# Author    : edmoura
+# Author    : eduardomourar
 # Desc      : Script para acessar o site do Consulado
 # Date      : 2016-09-28
-# Changed   : 2016-11-21
+# Changed   : 2016-12-31
 # ---------------------------------------------------------------------------------------
 # Copyright (C) 2016
 # ---------------------------------------------------------------------------------------
 
 import requests
+import logging
 import os
+
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
 
 print('----BEGIN----')
 
-USER_CPF = 'USER_CPF' # Substituir por dados reais
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+USER_CPF = 'USER_CPF' # Substituir por dados reais sem necessidade 
 USER_NAME = 'USER_NAME'
 USER_PASS = 'USER_PASS'
 
@@ -33,8 +50,24 @@ s = requests.Session()
 r = s.post(url, data=values)
 
 # Tabela de equivalecia do campo rbtnhora
-# rbtnhora	1		2		3		4		5		6		7		8		9		10		11		12		13		14		15		16		17		18		19		20		21		22		23		24		25		26		27		28		29		30		31		32		33
-# Horario	08:00	08:15	08:30	08:45	09:00	09:15	09:30	09:45	10:00	10:15	10:30	10:45	11:00	11:15	11:30	11:45	12:00	12:15	12:30	12:45	13:00	13:15	13:30	13:45	14:00	14:15	14:30	14:45	15:00	15:15	15:30	15:45	16:00
+# rbtnhora  Manha    rbtnhora  Horario Tarde
+# 1         08:00    17        12:00
+# 2         08:15    18        12:15
+# 3         08:30    19        12:30
+# 4         08:45    20        12:45
+# 5         09:00    21        13:00
+# 6         09:15    22        13:15
+# 7         09:30    23        13:30
+# 8         09:45    24        13:45
+# 9         10:00    25        14:00
+# 10        10:15    26        14:15
+# 11        10:30    27        14:30
+# 12        10:45    28        14:45
+# 13        11:00    29        15:00
+# 14        11:15    30        15:15
+# 15        11:30    31        15:30
+# 16        11:45    32        15:45
+#                    33        16:00
 
 # Passaporte Normal (com entrega de Cartão de Cidadão)
 # values = {
@@ -52,11 +85,9 @@ values = {
 }
 
 r = s.post(url, data=values, stream=True)
-r.raw
-with open('response.html', 'wb') as fd:
-    print('Output file: ' + os.path.join(os.getcwd(), fd.name))
-    for chunk in r.iter_content():
-        fd.write(chunk)
-        break
-    fd.close()
+if r.status_code == 200:
+    with open('response.html', 'wb') as fd:
+        print('Output HTTP response to file: ' + os.path.join(os.getcwd(), fd.name))
+        fd.write(r.content)
+        fd.close()
 print('----END----')
